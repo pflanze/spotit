10d9196958f6d7148c0cced61f62aa2829baabf9
Christian Jaeger <ch@christianjaeger.ch>
2019-09-10 21:09:52 +0100

    fix url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject spotit "0.0.1"
   :description ""
-  :url "http://github.com/pflanze/spotit"
+  :url "https://github.com/pflanze/spotit"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
